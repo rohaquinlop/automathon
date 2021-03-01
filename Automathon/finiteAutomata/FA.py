fd38684ebb0651852773c9a7f1de9bfa0f7db45e
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-03-01 13:26:48 -0500

    FA - Abstract Class

---
+++ Automathon/finiteAutomata/FA.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractMethod
+
+class FA(ABC):
+  """ Abstract class for representing Finite Automata"""
+
+  def validate(self, S : string):
+    pass
+  
+  def isValid(self):
+    pass
