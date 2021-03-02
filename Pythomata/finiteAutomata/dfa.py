3c69c481311abb2bea9acd0f49c7d173a7019001
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-03-02 14:01:59 -0500

    DFA - getInitialState

--- Pythomata/finiteAutomata/dfa.py
+++ Pythomata/finiteAutomata/dfa.py
@@ -1,7 +1,10 @@
 class DFA(self):
 
-  def __init__(self):
-    pass
+  def __init__(self, initialState: string):
+    self.initialState = initialState
   
+  def getInitialState(self):
+    return self.initialState
+
   def isValid(self) -> bool:
     pass
