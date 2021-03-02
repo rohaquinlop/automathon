74bc7e75c5caf9f9ce1bb526392ed8572abf97ca
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-03-02 15:45:29 -0500

    Create DA

---
+++ pythomata/finiteAutomata/dfa.py
@@ -0,0 +1,10 @@
+class DFA():
+
+  def __init__(self, initialState : str):
+    self.initialState = initialState
+
+  def getInitialState(self):
+    return self.initialState
+
+  def isValid(self) -> bool:
+    pass
