acd9a0c30a1ff4225bffd454d74c3f7c6c2eaa40
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-03-02 14:54:27 -0500

    Update test_finiteAutomata.py

--- tests/test_finiteAutomata.py
+++ tests/test_finiteAutomata.py
@@ -2,9 +2,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from pythomata.finiteAutomata import dfa
+from pythomata.finiteAutomata.dfa import *
 
 def test_dfa():
-  fa = dfa.DFA(initialState='q0')
+  fa = DFA(initialState='q0')
 
   assert fa.getInitialState() == 'q0'
