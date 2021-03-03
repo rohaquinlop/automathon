ace88c960cb716df9514e854bab96b893fdacdc2
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-03-02 21:19:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
-    name="pythomata",
-    packages=find_packages(include=['pythomata']),
+    name="automathon",
+    packages=find_packages(include=['automathon']),
     version='0.0.0',
     description="A Python library for simulating finite automata",
     author="Robin Hafid Quintero Lopez",
@@ -10,4 +10,15 @@ setup(
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
     test_suite='tests',
+    author_email='rohaquinlop301@gmail.com',
+    url='https://github.com/rohaquinlop/automathon',
+    download_url="https://github.com/rohaquinlop/automathon/archive/v_000.tar.gz",
+    keywords=["python", "automata", "automata", "automathon"],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 3'
+    ]
 )
