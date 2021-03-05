70a29823ba4f7619f91f9ffc94021f829c7bcb23
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-03-05 17:14:37 -0500

    test DFA isValid

--- tests/test_finiteAutomata.py
+++ tests/test_finiteAutomata.py
@@ -1,6 +1,8 @@
 from automathon.finiteAutomata.dfa import *
 
 def test_dfa():
-  fa = DFA({}, {}, dict(), 'q0', {})
+  fa = DFA({'q0', 'q1', 'q2'}, {'0', '1'},  { 'q0' : {'0' : 'q0', '1' : 'q1'},'q1' : {'0' : 'q2', '1' : 'q0'}, 'q2' : {'0' : 'q1', '1' : 'q2'} }, 'q0',
+        {'q0'})
 
-  assert fa.getInitialState() == 'q0'
+  assert fa.isValid() == True
+
