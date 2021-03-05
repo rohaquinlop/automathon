92b140a38454e3cfa458a7c497f194806b4acd86
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-03-05 02:56:05 -0500

    Documentation
    Documentation about the DFA Class

--- automathon/finiteAutomata/dfa.py
+++ automathon/finiteAutomata/dfa.py
@@ -1,7 +1,69 @@
 class DFA():
+  """A Class used to represent a Deterministic Finite Automaton
 
-  def __init__(self, initialState : str):
+  ...
+
+  Attributes
+  - - - - - - - - - - - - - - - - - -
+  Q : set
+    Set of strings where each string represent the states.
+    Ex:
+      Q = {'q0', 'q1', 'q2'}
+
+  sigma : set
+    Set of strings that represents the alphabet.
+    Ex:
+      sigma = {'0', '1'}
+  
+  delta : dict
+    Dictionary that represents the transition function.
+    Ex:
+      delta = {
+                'q0' : {'0' : 'q0', '1' : 'q1'},
+                'q1' : {'0' : 'q2', '1' : 'q0'},
+                'q2' : {'0' : 'q1', '1' : 'q2'},
+              }
+  
+  initialState : str
+    String that represents the initial state from where any input is processed (initialState ∈ Q / initialState in Q).
+    Ex:
+      initialState = 'q0'
+  
+  F : set
+    Set of strings that represent the final state/states of Q (F ⊆ Q).
+    Ex:
+      F = {'q0'}
+  
+
+  Methods
+  - - - - - - - - - - - - - - - - - -
+  """
+
+  def __init__(self, Q : set, sigma : set, delta : dict, initialState : str, F : set):
+    """
+    Parameters
+    - - - - - - - - - - - - - - - - - -
+    
+    Q : set
+      Set of strings where each string represent the states.
+    
+    sigma : set
+      Set of strings that represents the alphabet.
+    
+    delta : dict
+      Dictionary that represents the transition function.
+    
+    initialState : str
+      String that represents the initial state from where any input is processed (initialState ∈ Q / initialState in Q).
+    
+    F : set
+      Set of strings that represent the final state/states of Q (F ⊆ Q).
+    """
     self.initialState = initialState
+    self.sigma = sigma
+    self.delta = delta
+    self.initialState = initialState
+    self.F = F
 
   def getInitialState(self):
     return self.initialState
