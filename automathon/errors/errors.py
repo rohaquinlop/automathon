718aca3671bf7a4d83dcc04ed66ee73051d2ba4a
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-03-06 15:34:37 -0500

    Create Exceptions

---
+++ automathon/errors/errors.py
@@ -0,0 +1,21 @@
+class Error(Exception):
+  """Base class for exceptions in this module."""
+  pass
+
+class InputError(Error):
+  """Exception raised for errors in the input.
+
+  Attributes
+  - - - - - - - - - - - - - - - - - -
+
+    expression : str
+      Input expression in which the error ocurred.
+
+    message : str
+      Explaination of the error.
+  """
+
+  def __init__(self, expression, message):
+    self.expression = expression
+    self.message = message
+
