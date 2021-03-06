006612d923fea24ae0891dbdde82a3700218f49e
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-03-06 15:37:50 -0500

    Import Errors module

--- tests/test_finiteAutomata.py
+++ tests/test_finiteAutomata.py
@@ -1,4 +1,5 @@
 from automathon.finiteAutomata.dfa import *
+from automathon.errors.errors import *
 
 def test_dfa():
   fa = DFA({'q0', 'q1', 'q2'}, {'0', '1'},  { 'q0' : {'0' : 'q0', '1' : 'q1'},'q1' : {'0' : 'q2', '1' : 'q0'}, 'q2' : {'0' : 'q1', '1' : 'q2'} }, 'q0',
