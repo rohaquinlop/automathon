523599866fb19edeedc0df5da0ac5e62b56035ed
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-03-11 11:37:30 -0500

    raise InputError test - pytest

--- tests/test_finiteAutomata.py
+++ tests/test_finiteAutomata.py
@@ -1,5 +1,6 @@
 from automathon.finiteAutomata.dfa import *
 from automathon.errors.errors import *
+import pytest
 
 def test_dfa():
   fa = DFA({'q0', 'q1', 'q2'}, {'0', '1'},  { 'q0' : {'0' : 'q0', '1' : 'q1'},'q1' : {'0' : 'q2', '1' : 'q0'}, 'q2' : {'0' : 'q1', '1' : 'q2'} }, 'q0',
@@ -7,3 +8,11 @@ def test_dfa():
 
   assert fa.isValid() == True
 
+def test_InputError():
+  with pytest.raises(InputError) as excinfo:
+    
+    def f():
+      raise InputError('001', 'Is not valid')
+
+    f()
+  assert 'Is not valid' in str(excinfo.value)
