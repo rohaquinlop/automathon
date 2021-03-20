89dc414188f367e46c9b11b71b88e45a173e9b6d
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-03-20 05:16:07 -0500

    added accept tests

--- tests/test_finiteAutomata.py
+++ tests/test_finiteAutomata.py
@@ -2,11 +2,29 @@ from automathon.finiteAutomata.dfa import *
 from automathon.errors.errors import *
 import pytest
 
-def test_dfa():
-  fa = DFA({'q0', 'q1', 'q2'}, {'0', '1'},  { 'q0' : {'0' : 'q0', '1' : 'q1'},'q1' : {'0' : 'q2', '1' : 'q0'}, 'q2' : {'0' : 'q1', '1' : 'q2'} }, 'q0',
-        {'q0'})
+def test_dfa_isValid():
+  fa = DFA({'q0', 'q1', 'q2'}, {'0', '1'},  { 'q0' : {'0' : 'q0', '1' : 'q1'},'q1' : {'0' : 'q2', '1' : 'q0'}, 'q2' : {'0' : 'q1', '1' : 'q2'} }, 'q0', {'q0'})
 
   assert fa.isValid() == True
+
+def test_dfa_accept_Epsilon():
+  fa = DFA({'q0', 'q1', 'q2'}, {'0', '1'},  { 'q0' : {'0' : 'q0', '1' : 'q1'},'q1' : {'0' : 'q2', '1' : 'q0'}, 'q2' : {'0' : 'q1', '1' : 'q2'} }, 'q0', {'q0'})
+
+  assert fa.accept("") == True ##Epsilon
+
+def test_dfa_accept_input1():
+  fa = DFA({'q0', 'q1', 'q2'}, {'0', '1'},  { 'q0' : {'0' : 'q0', '1' : 'q1'},'q1' : {'0' : 'q2', '1' : 'q0'}, 'q2' : {'0' : 'q1', '1' : 'q2'} }, 'q0', {'q0'})
+
+  assert fa.accept("001001") == True
+
+def test_dfa_accept_input2():
+  fa = DFA({'q0', 'q1', 'q2'}, {'0', '1'},  { 'q0' : {'0' : 'q0', '1' : 'q1'},'q1' : {'0' : 'q2', '1' : 'q0'}, 'q2' : {'0' : 'q1', '1' : 'q2'} }, 'q0', {'q0'})
+
+  with pytest.raises(InputError) as excinfo:
+    fa.accept("102")
+  
+  assert 'Is not declared in sigma' in str(excinfo.value)
+
 
 def test_InputError():
   with pytest.raises(InputError) as excinfo:
