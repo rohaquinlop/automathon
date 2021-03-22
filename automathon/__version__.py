d853462c857927de16231011bc14d151615da874
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-03-22 16:32:47 -0500

    Package metadata

---
+++ automathon/__version__.py
@@ -0,0 +1,8 @@
+""""Package metadata."""
+
+__title__ = "automathon"
+__description__ = "A Python library for simulating finite automata"
+__url__ = "https://github.com/rohaquinlop/automathon.git"
+__version__ = "0.0.3"
+__author__ = "Robin Hafid Quintero Lopez"
+__author_email__ = "rohaquinlop301@gmail.com"
