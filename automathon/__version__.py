b6ef7f79a1639ce8ea3aa1f6270718487e7428af
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-03-25 11:21:32 -0500

    started v0.0.6

--- automathon/__version__.py
+++ automathon/__version__.py
@@ -3,6 +3,6 @@
 __title__ = "automathon"
 __description__ = "A Python library for simulating finite automata"
 __url__ = "https://github.com/rohaquinlop/automathon.git"
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 __author__ = "Robin Hafid Quintero Lopez"
 __author_email__ = "rohaquinlop301@gmail.com"
