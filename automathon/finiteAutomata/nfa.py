ce05a59921646092b91f5d0ad4aabf4ca371281d
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2021-07-16 03:03:20 -0500

    Fixed unreachable code on NFA isValid function

--- automathon/finiteAutomata/nfa.py
+++ automathon/finiteAutomata/nfa.py
@@ -4,7 +4,8 @@ from collections import deque
 from graphviz import Digraph
 from automathon.finiteAutomata.dfa import DFA
 
-class NFA():
+
+class NFA:
   """A Class used to represent a Non-Deterministic Finite Automaton
 
   ...
@@ -57,8 +58,8 @@ class NFA():
   accept(S : str) -> bool : Returns True if the given string S is accepted by the NFA
   complement() -> NFA : Returns the complement of the NFA
   """
-
-  def __init__(self, Q : set, sigma : set, delta : dict, initialState : str, F : set):
+  
+  def __init__(self, Q: set, sigma: set, delta: dict, initialState: str, F: set):
     """
     Parameters
     - - - - - - - - - - - - - - - - - -
@@ -83,8 +84,8 @@ class NFA():
     self.delta = delta
     self.initialState = initialState
     self.F = F
-
-  def accept(self, S : str) -> bool:
+  
+  def accept(self, S: str) -> bool:
     """ Returns True if the given string S is accepted by the NFA
 
     The string S will be accepted if ∀a · a ∈ S ⇒ a ∈ sigma, which means that all the characters in S must be in sigma (must be in the alphabet).
@@ -94,20 +95,20 @@ class NFA():
     S : str
       A string that the NFA will try to process.
     """
-
+    
     ## Basic Idea: Search through states (delta) in the NFA, since the initial state to the final states
-
+    
     ## BFS states
-
-    q = deque() ## queue -> states from i to last character in S | (index, state)
-    q.append( [0, self.initialState] ) ## Starts from 0
-    ans = False ## Flag
-
+    
+    q = deque()  ## queue -> states from i to last character in S | (index, state)
+    q.append([0, self.initialState])  ## Starts from 0
+    ans = False  ## Flag
+    
     while q and not ans:
       frontQ = q.popleft()
       idx = frontQ[0]
       state = frontQ[1]
-
+      
       if idx == len(S):
         if state in self.F:
           ans = True
@@ -118,89 +119,87 @@ class NFA():
         for transition in self.delta[state].items():
           d = transition[0]
           states = transition[1]
-
+          
           if d == "":
             ## Is epsilon
             for state in states:
-              #Do not consume character
-              q.append( [idx, state] )
+              # Do not consume character
+              q.append([idx, state])
           elif S[idx] == d:
             for state in states:
-              #Consume character
-              q.append( [idx+1, state] )
-
+              # Consume character
+              q.append([idx + 1, state])
+    
     if S == "":
       ans = True
-
+    
     return ans
-
-
+  
   def isValid(self) -> bool:
     """ Returns True if the NFA is an valid automata """
-
-    #Validate if the initial state is in the set Q
+    
+    # Validate if the initial state is in the set Q
     if self.initialState not in self.Q:
       raise SigmaError(self.initialState, 'Is not declared in Q')
-
-    #Validate if the delta transitions are in the set Q
+    
+    # Validate if the delta transitions are in the set Q
     for d in self.delta:
       if d != "" and d not in self.Q:
         raise SigmaError(d, 'Is not declared in Q')
-
-        #Validate if the d transitions are valid
-        for s in self.delta[d]:
-          if s != "" and s not in self.sigma:
-            raise SigmaError(s, 'Is not declared in sigma')
-          for q in self.delta[d][s]:
-            if q not in self.Q:
-              raise SigmaError(self.delta[d][s], 'Is not declared Q')
-
-
-    #Validate if the final state are in Q
+      
+      # Validate if the d transitions are valid
+      for s in self.delta[d]:
+        if s != "" and s not in self.sigma:
+          raise SigmaError(s, 'Is not declared in sigma')
+        for q in self.delta[d][s]:
+          if q not in self.Q:
+            raise SigmaError(self.delta[d][s], 'Is not declared Q')
+    
+    # Validate if the final state are in Q
     for f in self.F:
       if f not in self.Q:
         raise SigmaError(f, 'Is not declared in Q')
-
-    #None of the above cases failed then this NFA is valid
+    
+    # None of the above cases failed then this NFA is valid
     return True
-
+  
   def complement(self) -> 'NFA':
     """Returns the complement of the NFA."""
     Q = self.Q
     sigma = self.sigma
     delta = self.delta
     initialState = self.initialState
-    F = { state for state in self.Q if state not in self.F}
+    F = {state for state in self.Q if state not in self.F}
     
     return NFA(Q, sigma, delta, initialState, F)
   
   def getEClosure(self, q):
     ans = [q]
-
+    
     if q in self.delta:
       if '' in self.delta[q]:
         for st in self.delta[q]['']:
           ans.extend([k for k in self.getEClosure(st) if k not in ans])
     return ans
-
+  
   def removeEpsilonTransitions(self) -> 'NFA':
     """Returns a copy of the actual NFA that doesn't contain epsilon transitions"""
-
+    
     Qprime = []
     deltaPrime = dict()
-
+    
     eClosures = []
-
+    
     isolatedNodes = []
-
+    
     tmp = self.getEClosure(self.initialState)
     qVisiteds = tmp
-
+    
     if len(tmp) == 1:
       isolatedNodes.extend(tmp.copy())
     elif len(tmp) > 1:
       eClosures.append(tmp.copy())
-
+    
     for q in self.Q:
       tmp = []
       if q not in qVisiteds:
@@ -215,13 +214,13 @@ class NFA():
     nodeClosure = dict()
     valueClosure = dict()
     cnt = 0
-
+    
     for c in isolatedNodes:
       cnt += 1
       nodeClosure[c] = cnt
       valueClosure[cnt] = [c]
       Qprime.append([c])
-
+    
     for c in eClosures:
       cnt += 1
       for e in c:
@@ -230,11 +229,11 @@ class NFA():
         nodeClosure[e] = cnt
       valueClosure[cnt] = c
       Qprime.append(c)
-
+    
     auxDelta = dict()
-    for qs in Qprime: ## Qprime = [ [], [], ...]
+    for qs in Qprime:  ## Qprime = [ [], [], ...]
       ##Iterate over the states
-      auxDelta[str(qs)] = dict() ##Initialize the auxiliar delta
+      auxDelta[str(qs)] = dict()  ##Initialize the auxiliary delta
       for transitionValue in self.sigma:
         ## get the values
         if transitionValue != '':
@@ -252,8 +251,8 @@ class NFA():
       for transitionValue in auxDelta[qs]:
         deltaPrime[qs][transitionValue] = []
         for idValue in auxDelta[qs][transitionValue]:
-          deltaPrime[qs][transitionValue].append( str(valueClosure[idValue]) )
-
+          deltaPrime[qs][transitionValue].append(str(valueClosure[idValue]))
+    
     finalStates = set()
     for qs in Qprime:
       for q in qs:
@@ -270,15 +269,15 @@ class NFA():
       aux.add(str(qs))
     
     Qprime = aux.copy()
-
+    
     return NFA(Qprime, self.sigma, deltaPrime, str(initialState), finalStates)
-
+  
   def getDFA(self) -> DFA:
     """Convert the actual NFA to DFA and return it's conversion"""
-
+    
     localNFA = NFA(self.Q, self.sigma, self.delta, self.initialState, self.F)
     flag = False
-
+    
     ##verify if have epsilon transitions
     for q in self.Q:
       if q in self.delta and not flag:
@@ -286,26 +285,26 @@ class NFA():
           if transitionValue == '' and not flag:
             localNFA = self.removeEpsilonTransitions()
             flag = True
-
+    
     Qprime = []
     deltaPrime = dict()
-
+    
     queue = deque()
     visited = [[localNFA.initialState]]
     queue.append([localNFA.initialState])
-
+    
     while queue:
-      qs = queue.pop() ## state Q
-
-      T = dict() ## {str : list}
-
+      qs = queue.pop()  ## state Q
+      
+      T = dict()  ## {str : list}
+      
       for q in qs:
         if q in localNFA.delta:
           for s in localNFA.delta[q]:
             tmp = localNFA.delta[q][s].copy()
             if s in T:
               ## avoid add repeated values
-              T[s].extend( [k for k in tmp if k not in T[s]] )
+              T[s].extend([k for k in tmp if k not in T[s]])
             else:
               T[s] = tmp
       
@@ -321,7 +320,7 @@ class NFA():
       Qprime.append(qs)
     
     Fprime = set()
-
+    
     for qs in Qprime:
       for q in qs:
         if q in localNFA.F:
@@ -329,7 +328,6 @@ class NFA():
           break
     
     aux = set()
-
     
     for qs in Qprime:
       aux.add(str(qs))
@@ -337,8 +335,8 @@ class NFA():
     Qprime = aux
     
     return DFA(Qprime, localNFA.sigma, deltaPrime, str([localNFA.initialState]), Fprime)
-
-  def union(self, M : 'NFA') -> 'NFA':
+  
+  def union(self, M: 'NFA') -> 'NFA':
     """Given a NFA M returns the union automaton"""
     sigma = self.sigma.union(M.sigma)
     Q = set()
@@ -348,7 +346,7 @@ class NFA():
     realValueM = dict()
     selfDelta = dict()
     mDelta = dict()
-    ## Fix possible errors when using the dictonaries with the name of the states
+    ## Fix possible errors when using the dictionaries with the name of the states
     for i, q in enumerate(self.Q, 1):
       realValueSelf[q] = "q{}".format(i)
       Q.add(realValueSelf[q])
@@ -362,8 +360,8 @@ class NFA():
     
     for q in M.F:
       F.add(realValueM[q])
-
-    #Replace the values
+    
+    # Replace the values
     for q, transition in self.delta.items():
       ## q : string, transition : {string -> list(string)}
       tmpDict = dict()
@@ -386,28 +384,26 @@ class NFA():
         tmpDict[s] = tmpStates.copy()
       mDelta[realValueM[q]] = tmpDict.copy()
     
-    delta = {**selfDelta, **mDelta}
+    delta = {**selfDelta, **mDelta, initialState: {
+      '': [realValueSelf[self.initialState], realValueM[M.initialState]]}}
 
-    delta[initialState] = {
-      '' : [realValueSelf[self.initialState], realValueM[M.initialState]] }
-    
     return NFA(Q, sigma, delta, initialState, F)
-
-  def product(self, M : 'NFA') -> 'NFA':
+  
+  def product(self, M: 'NFA') -> 'NFA':
     """Given a DFA M returns the product automaton"""
     ##Using Demorgan's laws : AB = (A' + B')'
     ##TODO: Create tests
     nfa = self.complement().union(M.complement())
-
+    
     return nfa.complement()
-
+  
   def view(self, fileName: str):
     dot = Digraph(name=fileName, format='png')
     
     dot.graph_attr['rankdir'] = 'LR'
-
+    
     dot.node("", "", shape='plaintext')
-
+    
     for f in self.F:
       dot.node(f, f, shape='doublecircle')
     
